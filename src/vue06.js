04ae1a36fa61e87ba12bd7de6f6a3d21c2fbd404
acrens <chengr@dxyer.com>
2016-11-15 20:18:34 +0800

    vue 事件处理器

---
+++ src/vue06.js
@@ -0,0 +1,26 @@
+/**
+ * Vue 事件处理器、表单控件绑定
+ */
+var vm = new Vue({
+    el: '.J-dom',
+    data: {
+        counter: 0
+    },
+    methods: {
+        greet: function(e) {
+            alert(e.target.tagName);
+        },
+        warn: function(message, event) {
+
+            // 现在我们可以访问原生事件对象
+            if (event) event.preventDefault()
+            alert(message)
+        },
+        submit: function(e) {
+        	alert(e.target.value);
+        }
+    }
+});
+
+// 按键修饰符别名设置
+// Vue.config.keyCodes.f1 = 112;
