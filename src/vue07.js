76a5fbc7c5c8d9385d28c5178464c3adc18aeb2a
acrens <chengr@dxyer.com>
2016-11-16 15:57:56 +0800

    vue 组件

--- src/vue07.js
+++ src/vue07.js
@@ -8,12 +8,51 @@ Vue.component('component01', {
 
     // data 必须是一个函数
     data: function() {
-    	return {
-    		message: 'i love you...'
-    	};
+        return {
+            message: 'i love you...'
+        };
     }
 });
 
+Vue.component('button-counter', {
+    template: '<button v-on:click="increment">{{ counter }}</button>',
+    data: function() {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        increment: function() {
+            var that = this;
+            that.counter += 1;
+            that.$emit('increments');    // 触发父组件上的 increment 自定义事件
+        }
+    },
+});
+
+Vue.component('hello-world', {
+    template: '#hello-world-template'
+});
+
+Vue.component('my-input', {
+    template: '\
+        <div class="form-group">\
+          <label v-bind:for="randomId">{{ label }}:</label>\
+          <input v-bind:id="randomId" v-bind:value="value" v-on:input="onInput">\
+        </div>\
+    ',
+    props: ['value', 'label'],
+    data: function() {
+        return {
+            randomId: 'input-' + Math.random()
+        }
+    },
+    methods: {
+        onInput: function(event) {
+            this.$emit('input', event.target.value);    // 触发父组件 input
+        }
+    }
+});
 
 // 局部注册一个组件
 var Child = {
@@ -21,13 +60,22 @@ var Child = {
 
     // 声明 props（单向数据流）
     // 1、props 可以是一个对象，可以为每个属性指定类型、验证规则及是否必填等
-    props: ['result', 'dynamicResult']	// 父组件通过 props 传递参数给子组件（dynamicResult -》 dynamic-result)
+    props: ['result', 'dynamicResult'] // 父组件通过 props 传递参数给子组件（dynamicResult -》 dynamic-result)
+};
+
+var Child03 = {
+    template: '<div>local A custom component03 {{ result }} - {{ dynamicResult }}!</div>',
+
+    // 声明 props（单向数据流）
+    // 1、props 可以是一个对象，可以为每个属性指定类型、验证规则及是否必填等
+    props: ['result', 'dynamicResult'] // 父组件通过 props 传递参数给子组件（dynamicResult -》 dynamic-result)
 };
 
 var vm = new Vue({
     el: '.J-dom',
     data: {
-    	message: 'i love you'	// 动态改变子组件内容 - dynamicResult
+        message: 'i love you', // 动态改变子组件内容 - dynamicResult
+        currentView: 'component02'
     },
     methods: {
 
@@ -36,6 +84,27 @@ var vm = new Vue({
 
     },
     components: {
-        'component02': Child
+        'component02': Child,
+        'component03': Child03
     }
 });
+
+new Vue({
+    el: '#counter-event-example',
+    data: {
+        total: 0
+    },
+    methods: {
+        incrementTotal: function() {
+            this.total += 1;
+        }
+    }
+});
+
+new Vue({
+    el: '#v-model-example',
+    data: {
+        message: 'hello 你们在干嘛'
+    }
+});
+
